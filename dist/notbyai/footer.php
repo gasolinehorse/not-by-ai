0b3f3e6d750d5ffc407581b68653224f7af87c9e
Allen Hsu <allenwhsu@gmail.com>
2023-05-12 23:20:29 -0400

    add about page, disable css badge-grab

--- dist/notbyai/footer.php
+++ dist/notbyai/footer.php
@@ -1,7 +1,8 @@
-		<div class="row border-top">
+		<div class="row border-top text-center">
       <div class="col text-center py-3 text-center">
-        <a href="<? bloginfo('url') ?>/translate" class="display-6 pt-3 text-decoration-underline me-3">Help Us Translate</a>
-        <a href="<? bloginfo('url') ?>/feedback" class="display-6 pt-3 text-decoration-underline me-3">Feedback</a>
+        <a href="<? bloginfo('url') ?>/translate" class="display-6 pt-3 text-decoration-underline me-2 me-sm-3">Help Us Translate</a>
+        <a href="<? bloginfo('url') ?>/feedback" class="display-6 pt-3 text-decoration-underline me-2 me-sm-3">Feedback</a>
+        <a href="<? bloginfo('url') ?>/about" class="display-6 pt-3 text-decoration-underline me-2 me-sm-3">About Us</a>
         <a href="<? bloginfo('url') ?>/contact" class="display-6 pt-3 text-decoration-underline">Contact</a>
         <a href="https://notbyai.fyi"><img class="pt-3 d-block mx-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/img/written-by-human-not-by-ai-white.svg" alt="Written By Human, Not By AI"></a>
       </div>
--- dist/notbyai/nav-below.php
+++
@@ -1,5 +0,0 @@
-<?php $args = array(
-'prev_text' => sprintf( esc_html__( '%s older', 'blankslate' ), '<span class="meta-nav">&larr;</span>' ),
-'next_text' => sprintf( esc_html__( 'newer %s', 'blankslate' ), '<span class="meta-nav">&rarr;</span>' )
-);
-the_posts_navigation( $args );
