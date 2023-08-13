cd1db33587ce26ae7e44de7c551c3314cff3bc7e
Allen Hsu <allenwhsu@gmail.com>
2023-08-13 16:35:46 -0400

    add privacy

---
+++ dist/privacy-policy.php
@@ -0,0 +1,59 @@
+<?php
+$pageTitle = 'Privacy Policy of Not By AI — Add the Badge to Human-Created Content';
+$pageDes = 'Learn about the privacy policy of Not By AI.';
+include 'header.php';
+include 'top-nav.php';
+?>
+    <main class="row justify-content-center px-4 px-lg-0">
+      <header>
+        <h1>Privacy Policy</h1>
+      </header>
+      <section class="col-md-8 col-xl-6 text-start pt-4 pb-5">
+        <p class="text-center">Last updated: August 13, 2023</p>
+
+        <p>Thank you for visiting Not By AI (https://notbyai.fyi) ("the Website"). Your privacy is important to us, and we are committed to protect your personal information. This Privacy Policy outlines the types of information we collect, how we use and protect that information, and your rights in relation to your personal data.</p>
+
+        <h2 class="h4">Information We Collect</h2>
+
+        <ul>
+          <li class="pb-2">Personal Information: We may collect personal information that you provide voluntarily when you interact with the Website. This could include your name, email address, and any other information you choose to share.</li>
+
+          <li>Usage Information: We automatically collect certain information about your visit to the Website, such as your browser type, operating system, referring URLs, and pages visited. This information helps us analyze how visitors use the Website and improve its functionality.</li>
+        </ul>
+
+        <h2 class="h4">How We Use Your Information</h2>
+
+        <ul>
+          <li class="pb-2">Providing Services: We may use your personal information to respond to your inquiries, provide requested information, or offer services that you have requested.</li>
+
+          <li class="pb-2">Improving User Experience: We use usage information to analyze trends, monitor the effectiveness of the Website, and improve user experience.</li>
+
+          <li>Communication: With your consent, we may use your email address to send you updates, newsletters, or other relevant information about the Website.</li>
+        </ul>
+
+        <h2 class="h4">Cookies and Tracking Technologies</h2>
+
+        <p>We may use cookies and similar tracking technologies to collect and store information about your interactions with the Website. You can manage your cookie preferences through your browser settings. However, disabling cookies may affect your experience on the Website.</p>
+
+        <h2 class="h4">Sharing Your Information</h2>
+
+        <p>We do not sell, trade, or otherwise transfer your personal information to third parties without your explicit consent, except as required by law or to protect our rights.</p>
+
+        <h2 class="h4">Data Security</h2>
+
+        <p>We take reasonable measures to protect your personal information from unauthorized access, alteration, or disclosure. However, no data transmission over the internet or electronic storage method is completely secure, so we cannot guarantee absolute security.</p>
+
+        <h2 class="h4">Your Rights</h2>
+
+        <p>You have the right to access, update, and delete your personal information held by us. If you wish to exercise these rights or have any questions about our privacy practices, please contact us using the information provided below.</p>
+
+        <h2 class="h4">Changes to this Privacy Policy</h2>
+
+        <p>We reserve the right to update or modify this Privacy Policy at any time. We will notify you of significant changes by posting a prominent notice on the Website or by sending you an email. Your continued use of the Website after any changes indicates your acceptance of the revised Privacy Policy.</p>
+
+        <h2 class="h4">Contact Us</h2>
+
+        <p>If you have any questions or concerns about this Privacy Policy or your data, please contact us by email at hello@notbyai.fyi.</p>
+      </section>
+    </main>
+    <?php include 'footer.php'; ?>
