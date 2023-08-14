9bf9513a2b177494361841e9d9fce330964e02d7
Allen Hsu <allenwhsu@gmail.com>
2023-08-13 21:13:02 -0400

    update language file

--- dist/all-supported-languages.php
+++ dist/all-supported-languages.php
@@ -1,1 +1,1 @@
-English, Spanish, German, Korean, Simplified Chinese (CN), Traditional Chinese (TW), Japanese, French, Indonesian, Thai, Turkish, Portuguese, Vietnamese, Russian, Persian, Swedish, Catalan, and Esperanto.
+English, Spanish, German, Korean, Simplified Chinese (CN), Traditional Chinese (TW), Japanese, French, Indonesian, Thai, Turkish, Greek, Portuguese, Vietnamese, Russian, Persian, Swedish, Catalan, and Esperanto.
