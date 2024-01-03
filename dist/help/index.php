4e9c0d2f154db211743ef8dbaae9883e27d9bd9f
Allen Hsu <allenwhsu@gmail.com>
2024-01-02 21:36:05 -0500

    adjust spacing and border for help

--- dist/help/index.php
+++ dist/help/index.php
@@ -13,201 +13,201 @@ include '../top-nav.php';
       </section>
       <section class="col-lg-11 col-xl-10 text-start pt-4 px-md-3 px-lg-5 pt-2 pb-3 bg-light">
         <h2 class="py-3 text-center">Frequently Asked Questions</h2>
-        <div class="row border-bottom">
-          <div class="col-6 py-3 border-end">
+        <div class="row border-lg-bottom">
+          <div class="col-lg-6 py-2 py-lg-3 border-lg-end">
             <h3 class="h5"><a href="/help/which-image-file-format-should-i-use.php">Which Image File Format Should I Use?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
-          <div class="col-6 py-3">
+          <div class="col-6 py-2 py-lg-3">
             <h3 class="h5"><a href="/help/how-do-i-link-my-not-by-ai-badge.php">How Do I Link My Not By AI Badge?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
         </div>
-        <div class="row border-bottom">
-          <div class="col-6 py-3 border-end">
+        <div class="row border-lg-bottom">
+          <div class="col-lg-6 py-2 py-lg-3 border-lg-end">
             <h3 class="h5"><a href="/help/is-not-by-ai-anti-ai.php">Is Not By AI Anti-AI?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
-          <div class="col-6 py-3">
+          <div class="col-6 py-2 py-lg-3">
             <h3 class="h5"><a href="/help/which-not-by-ai-badge-should-i-use.php">Which Not by AI Badge Should I Use?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
         </div>
-        <div class="row border-bottom">
-          <div class="col-6 py-3 border-end">
+        <div class="row border-lg-bottom">
+          <div class="col-lg-6 py-2 py-lg-3 border-lg-end">
             <h3 class="h5"><a href="/help/how-do-you-ensure-not-by-ai-badge-usage-complies-with-the-not-by-ai-90-rule.php">How do you ensure Not By AI badge usage complies with the Not By AI 90% Rule?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
-          <div class="col-6 py-3">
+          <div class="col-6 py-2 py-lg-3">
             <h3 class="h5"><a href="/help/what-is-the-not-by-ai-90-rule.php">What Is the Not By AI 90% Rule?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
         </div>
-        <div class="row border-bottom">
-          <div class="col-6 py-3 border-end">
+        <div class="row border-lg-bottom">
+          <div class="col-lg-6 py-2 py-lg-3 border-lg-end">
             <h3 class="h5"><a href="/help/why-cant-i-customize-the-not-by-ai-badges-to-better-fit-my-content.php">Why Can't I Customize the Not By AI Badges to Better Fit My Content?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
-          <div class="col-6 py-3">
+          <div class="col-6 py-2 py-lg-3">
             <h3 class="h5"><a href="/help/why-do-you-need-my-email-to-download-the-badges.php">Why Do You Need My Email to Download the Badges?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
         </div>
-        <div class="row border-bottom">
-          <div class="col-6 py-3 border-end">
+        <div class="row border-lg-bottom">
+          <div class="col-lg-6 py-2 py-lg-3 border-lg-end">
             <h3 class="h5"><a href="/help/where-does-donation-money-go.php">Where Does the Donation Money Go?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
-          <div class="col-6 py-3">
+          <div class="col-6 py-2 py-lg-3">
             <h3 class="h5"><a href="/help/the-not-by-ai-badges-are-not-available-in-my-language.php">The Not By AI Badges Are Not Available in My Language, What Do I Do?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
         </div>
         <div class="row">
-          <div class="col-6 py-3 border-end">
+          <div class="col-lg-6 py-2 py-lg-3 border-lg-end">
             <h3 class="h5"><a href="/help/can-i-share-my-not-by-ai-project-with-you-and-the-community.php">I Am Using the Not By AI Badge on My Project. Can I Share It with You and the Community?</a> <img src="/img/download-not-by-ai-badge.svg" alt="" style="transform: rotate(-90deg);"></h3>
           </div>
-          <div class="col-6 py-3">
+          <div class="col-6 py-2 py-lg-3">
           </div>
         </div>
       </section>
       <section class="col-lg-11 col-xl-10 py-5 text-start">
         <div class="row py-2 justify-content-center">
           <div class="col-lg-9 text-center">
-            <h2 id="how-to" class="mt-4 pt-2">How to Add the Not By AI Badges</h2>
+            <h2 id="how-to" class="mt-lg-4 pt-2">How to Add the Not By AI Badges</h2>
             <p class="mb-4">Depending on how important it is to you to convey the message of Not By AI, you have the flexibility to choose where to display the Not By AI badge. Some users may prefer the badge to be prominent while the others opt for a non-intrusive approach. Below are our recommendations but feel free to alter them to meet your needs.</p>
             <img class="img-fluid mb-5" srcset="/img/not-by-ai-badge-how-to@2x.jpg 2x" src="/img/not-by-ai-badge-how-to.jpg" alt="">
             <p class="mb-1">1. First, download the badges on our <a href="https://notbyai.fyi/" class="text-decoration-underline">homepage</a> or <a href="https://notbyai.fyi/business" class="text-decoration-underline">Business</a> page.</p>
             <p>2. Follow the recommendations below based on your content type.</p>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Blog posts, recipes, guides, and reviews</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Writer badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Writer badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Writer badge at the beginning or the end of the content, in the author profile section, or on the author’s webpage. </p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Essays, research, reports, and other educational/academic content </h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Writer badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Writer badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Writer badge on the cover of your content, at the beginning of your content, or at the end of your content. </p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Books and magazines</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Writer badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Writer badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Writer badge on the cover, the book blurb on the back, at the beginning or the end of the content, in the book jacket flap copy, or in the author profile section. Also, display it on marketing materials, such as the website of the author or the book, is encouraged.</p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Code</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Writer badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Writer badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Writer badge on your personal website, GitHub page, or directly on the UI, website, or app so it is visible to the end users.</p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Emails</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Writer badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Writer badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Writer badge in your email signature or at the beginning of the email.</p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Digital art</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Artist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Artist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Artist badge on the artwork (or on the back), near the artwork, in the description section, or on the artist's profile or website. </p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Physical art and handcrafted products</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Artist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Artist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Artist badge on the front, side, or back of the artwork or product, on the plaque, tag, or on the package/cover of the art or product. Also consider displaying the badge on the website of the art or product and on marketing materials.</p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Comics</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Artist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Artist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Artist badge on the cover, the comic book blurb on the back, at the beginning or the end of the content, in the book jacket flap copy, or in the author profile section. Also, display it on marketing materials, such as the website of the author or the comics, is encouraged.</p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Photography</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Producer/Generalist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Producer/Generalist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Producer/Generalist badge on the photographer’s profile or website, on the photos, near the photos, or on plaques of the photos. </p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Music</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Producer/Generalist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Producer/Generalist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Producer/Generalist badge on the album cover art or back, artist profile image, artist profile cover art, or on the artist's profile or website. </p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Videos</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Producer/Generalist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Producer/Generalist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Producer/Generalist badge in the video intros/bumpers or outros, at a corner throughout the video, or on the website of the video or the director/producer/filmmaker. </p>
             </div>
           </div>
         </div>
-        <div class="border-bottom px-md-3 py-2 mb-3">
+        <div class="border-bottom border-bottom py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Podcasts</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Producer/Generalist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Producer/Generalist badge</p>
             </div>
             <div class="col-sm-8">
               <p>Display the Producer/Generalist badge on the podcast cover and/or on the podcast website. If there is a video component, consider including the badge in the video intros/bumpers, outros, at a corner throughout the video, or incorporate it as a physical element, such as a banner, in the studio or recording environment. Also, display it on marketing materials to amplify your marketing efforts.</p>
             </div>
           </div>
         </div>
-        <div class="px-md-3 py-2 mb-3">
+        <div class="py-3">
           <div class="row py-md-2">
             <div class="col-sm-4">
               <h4 class="ps-lg-5 mb-1">Overall creative approach</h4>
-              <p class="ps-lg-5 small opacity-50 mb-1">The Producer/Generalist badge</p>
+              <p class="ps-lg-5 small opacity-50 mb-0">The Producer/Generalist badge</p>
             </div>
             <div class="col-sm-8">
               <p>The Producer/Generalist badge can be used to indicate that your overall creative approach to content creation follows the <a href="/help/what-is-the-not-by-ai-90-rule.php" class="text-decoration-underline">Not By AI 90% Rule</a>. For example, apply the badge when the content produced by your creative agency—whether it involves the design and copy of an app, website, or brochure—or in the case of your book, which may include both text and illustrations, meets the Not By AI requirement. Determine badge implementation by referring to the instructions above or consult with your design professionals.</p>
