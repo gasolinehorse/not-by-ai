6b48472a13cd04b746f27aa702a4333cbd1a31e0
Allen Hsu <allenwhsu@gmail.com>
2024-01-29 23:46:39 -0500

    change individual to individuals

--- dist/top-nav-dark.php
+++ dist/top-nav-dark.php
@@ -11,7 +11,7 @@
           </span>
         </div>
         <div class="col-6 col-sm-4 text-end mt-sm-2 pt-2 pt-sm-3 order-sm-3 d-none d-sm-block pe-4">
-        <a class="me-4 me-lg-5 me-sm-3 d-none d-md-inline-block text-center px-2 rounded-2 py-1 px-3 xs text-white opacity-50" href="https://notbyai.fyi/">Not By AI for Individual</a>
+        <a class="me-4 me-lg-5 me-sm-3 d-none d-md-inline-block text-center px-2 rounded-2 py-1 px-3 xs text-white opacity-50" href="https://notbyai.fyi/">Not By AI for Individuals</a>
       </div>
         <div class="col-8 col-sm-4 text-center order-sm-2">
           <img class="mt-4 mb-5" width="268" height="28.36" src="/img/not-by-ai-for-business.svg" alt="Not By AI for business">
