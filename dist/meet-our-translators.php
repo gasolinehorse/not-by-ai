130c0a64db98aca7642eb57f84831279c8925fff
Allen Hsu <allenwhsu@gmail.com>
2024-03-03 23:12:54 -0500

    release polish badges

--- dist/meet-our-translators.php
+++ dist/meet-our-translators.php
@@ -83,7 +83,7 @@ include 'top-nav.php';
             <p class="mb-0"><a class="text-decoration-underline" href="https://sweetgood.de" target="_blank" rel="nofollow">Christian Süßenguth</a></p>
           </div>
           <div class="col-12 pt-4">
-            <p class="opacity-50">...and other 25 anonymous translators.</p>
+            <p class="opacity-50">...and other 26 anonymous translators.</p>
           </div>
           <div class="overflow-hidden pt-4">
             <div class="sliding-background sliding-background--trans"></div>
