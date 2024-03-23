5c66808926b9c3b54f110f4a23ff472f59ad8d78
Allen Hsu <allenwhsu@gmail.com>
2024-03-23 23:39:06 +0000

    add new translator

--- dist/meet-our-translators.php
+++ dist/meet-our-translators.php
@@ -86,7 +86,7 @@ include 'top-nav.php';
             <p class="mb-0"><a class="text-decoration-underline" href="https://dominik.meller.net.pl/" target="_blank" rel="nofollow">Dominik Meller</a></p>
           </div>
           <div class="col-12 pt-4">
-            <p class="opacity-50">...and other 26 anonymous translators.</p>
+            <p class="opacity-50">...and other 27 anonymous translators.</p>
           </div>
           <div class="overflow-hidden pt-4">
             <div class="sliding-background sliding-background--trans"></div>
