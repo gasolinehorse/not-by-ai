f31706bf70fc06a332aea847422d14ecb48fe359
Allen Hsu <allenwhsu@gmail.com>
2024-04-06 21:04:51 -0400

    refresh project pages

--- dist/hi/sweetgood/index.php
+++ dist/hi/sweetgood/index.php
@@ -1,119 +1,19 @@
 <?php
-$pageTitle = 'SWEETGOOD | Not By AI';
-$pageDes = 'Download and add the Not By AI badge to showcase your AI-free & human-first approach to content creation (writings, art, photos, paintings, videos, and music)';
-include '../../header.php';
-include '../../top-nav.php';
+$project_name = 'SWEETGOOD';
+$project_author = 'Christian Süßenguth';
+$project_about = '🇩🇪 Alle Inhalte wurden zu 100% von Menschen und ohne jeglichen Einsatz von künstlicher Intelligenz erstellt. Das gilt auch für alle Grafiken auf der Seite. <br><br>🇺🇸 All content was 100% created by humans and without any use of artificial intelligence. This also applies to any graphics on the page.';
+$project_category = 'Technology and Software';
+$project_location = 'Pleinfeld, Germany';
+$project_url = 'sweetgood.de';
+$project_date = 'February 22, 2024';
+
+$project_badge_art = "";
+$project_badge_writing = "d-inline-block";
+$project_badge_production = "d-inline-block";
+
+$project_use_research = "";
+$project_use_inspiration = "";
+$project_use_proofing = "";
+$project_use_none = "d-block";
+include '../template.php';
 ?>
-    <main>
-      <header class="row no-md-bg justify-content-center text-start pt-1 pb-3 mb-3 mb-md-5 bg-light" style="margin-top: -1.8rem; background-image: url(/img/creator-profile-of-not-by-ai.svg); background-position: bottom right; background-repeat: no-repeat;">
-        <div class="col-md-11 col-xl-8 pt-1">
-          <div class="row position-relative z-1">
-            <div class="col">
-              <small class="xs"><a href="https://notbyai.fyi" class="text-decoration-underline">Home</a> <svg width="5" height="7" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.793 3.5.646 1.354l.708-.708L4.207 3.5 1.354 6.354l-.708-.708L2.793 3.5Z" fill="#000"/></svg> Creations <svg width="5" height="7" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.793 3.5.646 1.354l.708-.708L4.207 3.5 1.354 6.354l-.708-.708L2.793 3.5Z" fill="#000"/></svg> SWEETGOOD</small>
-            </div>
-          </div>
-          <div class="row justify-content-start pt-3 pb-2">
-            <div class="col-auto">
-              <img width="95" class="border rounded img-fluid position-relative z-1" src="/hi/sweetgood/img/logo.png" alt="logo">
-            </div>
-            <div class="col-auto">
-              <h1 class="h5 mt-1 mb-0 ls-normal" style="letter-spacing: 0;">Not By AI - Creator Profile</h1>
-              <h2 class="display-2 my-0 lh-sm" style="background-color: #f9f8f7; box-shadow: #f9f8f7 10px 0px 40px 40px;">SWEETGOOD <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><path fill="#007CE4" fill-rule="evenodd" d="m9.545 18.48-1.45.885a1.781 1.781 0 0 1-2.616-.952l-.543-1.61a.871.871 0 0 0-.695-.584l-1.68-.254a1.782 1.782 0 0 1-1.392-2.412l.62-1.582a.871.871 0 0 0-.158-.894L.507 9.803A1.782 1.782 0 0 1 .991 7.06l1.492-.814a.872.872 0 0 0 .454-.786l-.042-1.699a1.782 1.782 0 0 1 2.133-1.79l1.666.336c.32.065.65-.055.853-.31L8.607.67a1.782 1.782 0 0 1 2.784 0l1.06 1.328a.872.872 0 0 0 .854.31l1.665-.335a1.781 1.781 0 0 1 2.133 1.79l-.041 1.698a.872.872 0 0 0 .454.786l1.492.814a1.782 1.782 0 0 1 .483 2.742l-1.123 1.274a.871.871 0 0 0-.158.894l.62 1.582a1.782 1.782 0 0 1-1.392 2.412l-1.68.254a.871.871 0 0 0-.696.583l-.542 1.61a1.782 1.782 0 0 1-2.616.953l-1.45-.885a.872.872 0 0 0-.909 0Z" clip-rule="evenodd"/><path fill="#fff" fill-rule="evenodd" d="m15.083 8.685-5.982 5.983-4.185-4.185 1.506-1.506 2.679 2.678 4.476-4.476 1.506 1.506Z" clip-rule="evenodd"/></svg></h2>
-              <small class="xs">By Christian Süßenguth</small>
-            </div>
-          </div>
-        </div>
-      </header>
-      <div class="row justify-content-center">
-        <div class="col-md-7 col-xl-5 text-start px-4 px-lg-0 pt-3 pt-md-2 order-md-3">
-          <section class="border-bottom pb-4 position-relative">
-            <div class="row mx-0 border-bottom mb-4">
-              <div class="col-1 pe-1">
-                <img class="img-fluid" alt="" src="/img/not-by-ai-quote.svg">
-              </div>
-              <div class="col-11">
-                <h3 class="display-3 pb-3">Christian Süßenguth affirms that the applicable content of SWEETGOOD is majorly or entirely created by one or more humans. It adheres to the <a href="https://notbyai.fyi/help/what-is-the-not-by-ai-90-rule">Not By AI 90% Rule</a> and has undergone human review.<sup>1</sup></h3>
-              </div>
-            </div>
-            <h5 class="d-sm-inline mb-3">Applicable badge(s):</h5>
-            <img class="ms-3 my-3 my-sm-0 d-block d-sm-inline-block" src="/img/written-by-human-not-by-ai-white.svg" alt="Written by Human, Not By AI">
-            <img class="ms-3 my-3 my-sm-0 d-block d-sm-inline-block" src="/img/produced-by-human-not-by-ai-white.svg" alt="Produced by Human, Not By AI">
-          </section>
-          <section>
-            <h4 class="h3 pt-4 pt-md-5">About SWEETGOOD</h4>
-            <p class="mb-2">🇩🇪 Alle Inhalte wurden zu 100% von Menschen und ohne jeglichen Einsatz von künstlicher Intelligenz erstellt. Das gilt auch für alle Grafiken auf der Seite. </p>
-            <p>🇺🇸 All content was 100% created by humans and without any use of artificial intelligence. This also applies to any graphics on the page.</p>
-          </section>
-          <ol class="source pt-5 pb-3">
-            <li>
-              <cite>The use of the Not By AI badges and the human review process do not guarantee that the content is not majorly created by AI, we are dedicated to enhancing our verification process and credibility.</cite>
-            </li>
-          </ol>
-        </div>
-        <div class="col-md-1 border-start order-2" style="transform: translateX(40px);"></div>
-        <aside class="col-md-3 col-xl-2 text-start px-4 px-md-0 order-md-1">
-          <div class="nav flex-column position-sticky top-0 pt-md-3 pe-md-4 creator-profile">
-            <div class="row pb-3 px-2 mb-3">
-              <div class="col">
-                <div class="h5 pb-1">Project Details</div>
-                <div class="pb-1 pb-lg-3 pt-2">
-                  <div class="creator-profile__label">
-                    Industry
-                  </div>
-                  <div class="creator-profile__field">
-                    Technology and Software
-                  </div>
-                </div>
-                <div class="pb-1 pb-lg-3 pt-2">
-                  <div class="creator-profile__label">
-                    Location
-                  </div>
-                  <div class="creator-profile__field">
-                    Pleinfeld, Germany
-                  </div>
-                </div>
-                <div class="pb-1 pb-lg-3 pt-2">
-                  <div class="creator-profile__label">
-                    Applicable Content
-                  </div>
-                  <div class="creator-profile__field">
-                    <ul class="mb-0">
-                      <li>Writing</li>
-                      <li>Production/General</li>
-                    </ul>
-                  </div>
-                </div>  
-                <div class="pb-1 pb-lg-3 pt-2">
-                  <div class="creator-profile__label">
-                    IA Use Is Limited to 
-                  </div>
-                  <div class="creator-profile__field">
-                    None
-                  </div>
-                </div>
-                <div class="pb-1 pb-lg-3 pt-2">
-                  <div class="creator-profile__label">
-                    Website
-                  </div>
-                  <div class="creator-profile__field">
-                    <a class="text-decoration-underline" href="https://sweetgood.de" target="_blank">sweetgood.de</a>
-                  </div>
-                </div>
-                <div class="pb-3 pt-3">
-                  <div class="xs opacity-50 lh-sm">
-                    A Not By AI Project since 02/22/2024
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </aside>
-      </div>
-      <div class="row">
-        <div class="position-relative overflow-hidden w-100 mt-5 pt-4 border-bottom">
-          <div class="xs bg-white position-absolute z-1 bottom-0 start-50 translate-middle-x px-2 py-1 mb-3 rounded-2">Show your creativity with the Not By AI badges. <a href="https://notbyai.fyi" class="text-decoration-underline">Download free</a> <img src="/img/download-not-by-ai-badge.svg" alt=""></div>
-          <div class="sliding-background-l position-relative z-0"></div>
-        </div>
-      </div>
-    </main>
-    <?php include '../../footer.php'; ?>
