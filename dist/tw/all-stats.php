07dddc04348819a9123f0d957d15644d87dcd075
Allen Hsu <allenwhsu@gmail.com>
2024-06-01 22:03:05 -0400

    update stats

--- dist/tw/all-stats.php
+++ dist/tw/all-stats.php
@@ -14,7 +14,7 @@ const observer = new IntersectionObserver(entries => {
 observer.observe(counterElement);
 
 function startCounterAnimation() {
-  const targetNumber = 28; // Replace with new number
+  const targetNumber = 26; // Replace with new number
   let currentNumber = 0;
   const duration = 2400;
   const startTime = performance.now();
