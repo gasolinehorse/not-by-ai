fe8e552052bb31e5a32c678c74fee430a5ff94fc
Allen Hsu <allenwhsu@gmail.com>
2024-06-19 21:58:05 -0400

    add new social channels

--- dist/all-social.php
+++ dist/all-social.php
@@ -1,8 +1,34 @@
 <h3 class="pt-5 mb-0 mt-5 border-top fs-5">Follow Not By AI on Social Media</h3>
 <p class="pb-2 opacity-50 small">We share the coolest content. Check it out yourself!</p>
 <div class="pb-2">
-  <a href="https://twitter.com/NotByAIBadge" target="_blank"><img height="35" class="mx-3 share__tt" src="/img/twitter.svg" alt="share on Twitter"></a>
-  <a href="https://www.facebook.com/notbyai" target="_blank"><img height="35" class="mx-3 share__fb" src="/img/facebook.svg" alt="share on Facebook"></a>
-  <a href="https://www.linkedin.com/company/notbyai/" target="_blank"><img height="35" class="mx-3 share__li" src="/img/linkedin.svg" alt="share on LinkedIn"></a>
+  <a class="rounded-5 border d-inline-block social-ball-l" href="https://twitter.com/NotByAIBadge" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__tt" src="/img/twitter.svg" alt="share on X"></a>
+  <a class="rounded-5 border d-inline-block" href="https://www.instagram.com/notbyai/" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/instagram.svg" alt="share on Instagram"></a>
+  <a class="rounded-5 border d-inline-block" href="https://www.facebook.com/notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__fb" src="/img/facebook.svg" alt="share on Facebook"></a>
+  <a class="rounded-5 border d-inline-block" href="https://www.linkedin.com/company/notbyai/" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/linkedin.svg" alt="share on LinkedIn"></a>
+  <a class="rounded-5 border d-inline-block social-ball-r" href="https://www.pinterest.com/notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/pinterest.svg" alt="share on Pinterest"></a>
 </div>
+<style>
+  @media (min-width: 480px) {
+    @keyframes swing-l {
+      0% { transform: rotate(5deg) translateX(-18px); }
+      25% { transform: rotate(0deg) translateX(0); }
+      50% { transform: rotate(0deg) translateX(0); }
+      75% { transform: rotate(0deg) translateX(0); }
+      100% { transform: rotate(5deg) translateX(-18px); }
+    }
+    @keyframes swing-r {
+      0% { transform: rotate(0deg) translateX(0); }
+      25% { transform: rotate(0deg) translateX(0); }
+      50% { transform: rotate(-5deg) translateX(18px); }
+      75% { transform: rotate(0deg) translateX(0); }
+      100% { transform: rotate(0deg) translateX(0); }
+    }
+    .social-ball-l {
+      animation: swing-l 2s ease-in-out infinite;
+    }
+    .social-ball-r {
+      animation: swing-r 2s ease-in-out infinite;
+    }
+  }
+</style>
 <a href="https://notbyai.fyi" class="btn mt-4 mb-4">Return to Homepage</a>
