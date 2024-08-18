9fe9cf6d52b91f8f83e5d4a72278ff8c0a3f46ad
Allen Hsu <allenwhsu@gmail.com>
2024-08-18 13:43:24 -0400

    fix mobile social margin

--- dist/all-social.php
+++ dist/all-social.php
@@ -1,12 +1,12 @@
 <h3 class="pt-5 mb-0 mt-5 border-top fs-5">Follow Not By AI on Social Media</h3>
 <p class="pb-2 opacity-50 small">We share the coolest content. Check it out yourself!</p>
 <div class="pb-2">
-  <a class="rounded-5 border d-inline-block social-ball-l" href="https://twitter.com/NotByAIBadge" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__tt" src="/img/twitter.svg" alt="share on X"></a>
-  <a class="rounded-5 border d-inline-block" href="https://www.instagram.com/notbyai/" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/instagram.svg" alt="share on Instagram"></a>
-  <a class="rounded-5 border d-inline-block" href="https://www.facebook.com/notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__fb" src="/img/facebook.svg" alt="share on Facebook"></a>
-  <a class="rounded-5 border d-inline-block" href="https://www.linkedin.com/company/notbyai/" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/linkedin.svg" alt="share on LinkedIn"></a>
-  <a class="rounded-5 border d-inline-block" href="http://mastodon.social/@notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/mastodon.svg" alt="share on Mastodon"></a>
-  <a class="rounded-5 border d-inline-block social-ball-r" href="https://www.pinterest.com/notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/pinterest.svg" alt="share on Pinterest"></a>
+  <a class="rounded-5 border d-inline-block mb-2 mb-md-0 mx-2 mx-md-0 social-ball-l" href="https://twitter.com/NotByAIBadge" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__tt" src="/img/twitter.svg" alt="share on X"></a>
+  <a class="rounded-5 border d-inline-block mb-2 mb-md-0 mx-2 mx-md-0" href="https://www.instagram.com/notbyai/" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/instagram.svg" alt="share on Instagram"></a>
+  <a class="rounded-5 border d-inline-block mb-2 mb-md-0 mx-2 mx-md-0" href="https://www.facebook.com/notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__fb" src="/img/facebook.svg" alt="share on Facebook"></a>
+  <a class="rounded-5 border d-inline-block mb-2 mb-md-0 mx-2 mx-md-0" href="https://www.linkedin.com/company/notbyai/" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/linkedin.svg" alt="share on LinkedIn"></a>
+  <a class="rounded-5 border d-inline-block mb-2 mb-md-0 mx-2 mx-md-0" href="http://mastodon.social/@notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/mastodon.svg" alt="share on Mastodon"></a>
+  <a class="rounded-5 border d-inline-block mb-2 mb-md-0 mx-2 mx-md-0 social-ball-r" href="https://www.pinterest.com/notbyai" target="_blank"><img height="35" style="min-width: 35px;" class="m-3 share__li" src="/img/pinterest.svg" alt="share on Pinterest"></a>
 </div>
 <style>
   @media (min-width: 480px) {
