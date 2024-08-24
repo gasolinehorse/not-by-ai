ff1f85d686a6e8c50f552305579132b61547487b
Allen Hsu <allenwhsu@gmail.com>
2024-08-23 23:58:08 -0400

    add new lang

--- dist/meet-our-translators.php
+++ dist/meet-our-translators.php
@@ -115,7 +115,7 @@ include 'top-nav.php';
             <p class="mb-0"><a class="text-decoration-underline" href="https://www.linkedin.com/in/vasyl-pashko/" target="_blank" rel="nofollow">Vasyl Pashko</a></p>
           </div>
           <div class="col-12 pt-4">
-            <p class="opacity-50">...and other 31 anonymous translators.</p>
+            <p class="opacity-50">...and other 32 anonymous translators.</p>
           </div>
           <div class="overflow-hidden pt-4">
             <div class="sliding-background sliding-background--trans"></div>
