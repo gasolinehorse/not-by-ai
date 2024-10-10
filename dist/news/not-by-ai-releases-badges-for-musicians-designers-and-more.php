3d893d30b721f29c8338eae7712b0e1428b0edc5
Allen Hsu <allenwhsu@gmail.com>
2024-10-09 23:11:40 -0400

    hide announcement on press

--- dist/news/not-by-ai-releases-badges-for-musicians-designers-and-more.php
+++ dist/news/not-by-ai-releases-badges-for-musicians-designers-and-more.php
@@ -6,6 +6,7 @@ include '../top-nav.php';
 ?>
     <style>
       .top-nav-biz-link {display: none !important;} #topNav {margin-bottom:0px !important;}
+      #topNav div:last-child {display: none !important;}
     </style>
     <main>
       <header class="row no-md-bg justify-content-center text-start pt-1 pb-2 mb-3 mb-md-5 bg-light" style="background-image: url(/img/creator-profile-of-not-by-ai.svg); background-position: top right; background-repeat: no-repeat; background-attachment: fixed;">
