355d4478331e68e5f4d17085a28524432a02bdf5
Allen Hsu <allenwhsu@gmail.com>
2025-10-23 21:35:15 -0400

    add washington post news item, rename h1

--- dist/newsroom.php
+++ dist/newsroom.php
@@ -10,11 +10,27 @@ include './top-nav.php';
     <main class="">
       <header class="row no-md-bg justify-content-center text-start pt-1 pb-2 mb-3 mb-md-5 bg-light" style="background-image: url(/img/creator-profile-of-not-by-ai.svg); background-position: top right; background-repeat: no-repeat; background-attachment: fixed;">
         <div class="col-lg-11 col-xl-8 pt-3 pb-1">
-          <h1 class="text-start h4">Newsroom <span class="fw-normal xs opacity-50"><span class="px-3">|</span><a href="#news" class="text-decoration-underline">Latest News</a><span class="px-3">|</span><a href="#press" class="text-decoration-underline">Press Releases</a></span></h1>
+          <h1 class="text-start h4">Newsroom <span class="fw-normal xs opacity-50"><span class="px-3">|</span><a href="#news" class="text-decoration-underline">In the News</a><span class="px-3">|</span><a href="#press" class="text-decoration-underline">Press Releases</a></span></h1>
         </div>
       </header>
       <div class="container">
-        <h2 id="news" class="h3 text-center mb-4 mt-5 pt-md-3" style="scroll-margin-top: 4.5rem;">Latest News</h2>
+        <h2 id="news" class="h3 text-center mb-4 mt-5 pt-md-3" style="scroll-margin-top: 4.5rem;">In the News</h2>
+        <div class="row justify-content-center text-start px-4 px-lg-0 mb-4 mb-lg-5">
+          <div class="col-lg-11 col-xl-8 border-top pt-4">
+            <div class="row align-content-around">
+              <div class="col-md-3">
+                <p class="small">October 23, 2025</p>
+              </div>
+              <div class="col-md-6 pb-3">
+                <h2>Meet the People Who Dare to Say No to Artificial Intelligence</h2>
+                <a target="_blank" class="text-decoration-underline small" href="https://www.washingtonpost.com/technology/2025/10/23/opt-out-ai-workers-school/">Read More</a>
+              </div>
+              <div class="col-md-3">
+                <h3 class="h4 py-5 text-center rounded" style="background: linear-gradient(145deg, #f4f3f2 10%, #f9f8f7 10.5% 19%, transparent 19.5% 80.5%, #f9f8f7 81% 89.5%, #ffffff 90%), linear-gradient(145deg, #f4f3f2 10%, #f9f8f7 10.5% 19%, transparent 19.5% 80.5%, #f9f8f7 81% 89.5%, #ffffff 90%) 27.5px 55px, linear-gradient(35deg, #f4f3f2 10%, #f9f8f7 10.5% 19%, transparent 19.5% 80.5%, #f9f8f7 81% 89.5%, #ffffff 90%), linear-gradient(35deg, #f4f3f2 10%, #f9f8f7 10.5% 19%, transparent 19.5% 80.5%, #f9f8f7 81% 89.5%, #ffffff 90%) 27.5px 55px, conic-gradient(from -90deg at 37.5% 50%, transparent 75%, #f4f3f2 0) 6.875px 0, conic-gradient(from -90deg at 37.5% 50%, transparent 75%, #ffffff 0) 27.5px 0, linear-gradient(90deg, #ffffff 38%, #f4f3f2 0 50%, #ffffff 0 62%, #f4f3f2 0); background-size: 55px calc(2 * 55px / 3);">The Washington Post</h3>
+              </div>
+            </div>
+          </div>
+        </div>
         <div class="row justify-content-center text-start px-4 px-lg-0 mb-4 mb-lg-5">
           <div class="col-lg-11 col-xl-8 border-top pt-4">
             <div class="row align-content-around">
@@ -122,7 +138,7 @@ include './top-nav.php';
                 <a target="_blank" class="text-decoration-underline small" href="https://ideasforgood.jp/2023/03/29/not-by-ai/">Read More</a>
               </div>
               <div class="col-md-3">
-                <h3 class="h4 py-5 text-center rounded" style="background-color: #f9f8f7; opacity: 0.8; background-image: linear-gradient(0deg, #f9f8f7 50%, #F4F3F2 50%); background-size: 10px 10px;">IDEAS FOR GOOD</h3>
+                <h3 class="h4 py-5 text-center rounded" style="background: radial-gradient(50% 50% at 100% 0,   #f9f8f7 0% 5%, #f4f3f2 6% 15%, #f9f8f7 16% 25%, #f4f3f2 26% 35%, #f9f8f7 36% 45%,   #f4f3f2 46% 55%, #f9f8f7 56% 65%, #f4f3f2 66% 75%, #f9f8f7 76% 85%, #f4f3f2 86% 95%, transparent 96% ), radial-gradient(50% 50% at 0 100%,   #f9f8f7 0% 5%, #f4f3f2 6% 15%, #f9f8f7 16% 25%, #f4f3f2 26% 35%, #f9f8f7 36% 45%,   #f4f3f2 46% 55%, #f9f8f7 56% 65%, #f4f3f2 66% 75%, #f9f8f7 76% 85%, #f4f3f2 86% 95%, transparent 96% ), radial-gradient(50% 50%,   #f9f8f7 0% 5%, #f4f3f2 6% 15%, #f9f8f7 16% 25%, #f4f3f2 26% 35%, #f9f8f7 36% 45%,   #f4f3f2 46% 55%, #f9f8f7 56% 65%, #f4f3f2 66% 75%, #f9f8f7 76% 85%, #f4f3f2 86% 95%, transparent 96% ), radial-gradient(50% 50%,   #f9f8f7 0% 5%, #f4f3f2 6% 15%, #f9f8f7 16% 25%, #f4f3f2 26% 35%, #f9f8f7 36% 45%,   #f4f3f2 46% 55%, #f9f8f7 56% 65%, #f4f3f2 66% 75%, #f9f8f7 76% 85%, #f4f3f2 86% 95%, transparent 96% ) 45.5px 45.5px #f9f8f7; background-size: 91px 91px;">IDEAS FOR GOOD</h3>
               </div>
             </div>
           </div>
